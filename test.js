d22b594f6578f1a696e941f163e8913b5cce75d9
divramhit <ramhit828@gmail.com>
2022-02-22 09:50:49 +0400

    Fixed jankiness of mobile nav bar when resizing window

--- test.js
+++ test.js
@@ -11,4 +11,13 @@ navToggle.addEventListener('click', () => {
     primaryNav.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', false);
   }
-})
+});
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
