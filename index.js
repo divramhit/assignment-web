3cf476da386f58937c34ed554b34739638135769
divramhit <68056928+divramhit@users.noreply.github.com>
2022-03-16 08:37:10 +0400

    Major Update--> Stability, Side bar, Scrolling, Overscrolling, Prevent Body scrolling when side bar is up/scrolling

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const primaryNav = document.querySelector('.nav-links');
+//The code Below is used to Toggle the Side Menu from the Menu when in small screen sizes
+const sidebar = document.querySelector('.side-nav-links');
 const navToggle = document.querySelector('.mobile-nav-toggle');
+const body = document.body;
 
 navToggle.addEventListener('click', () => {
-  const visibility = primaryNav.getAttribute('data-visible');
+  const visibility = sidebar.getAttribute('data-visible');
+
 
   if (visibility == "false"){
-    primaryNav.setAttribute('data-visible', true);
+    sidebar.setAttribute('data-visible', true);
     navToggle.setAttribute('aria-expanded', true);
   } else if (visibility == "true"){
-    primaryNav.setAttribute('data-visible', false);
+    sidebar.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', false);
+  }
+  const sidebaropen = navToggle.getAttribute('aria-expanded');
+  // alert(sidebaropen);
+
+  if (sidebaropen == "true")
+  {
+    body.classList.toggle('noscroll', true);
+  }
+  else {
+    body.classList.toggle('noscroll', false);
   }
 });
 
+//This part is only to prevent glitches when resizing a window as it prevents any animation from running
 let resizeTimer;
 window.addEventListener("resize", () => {
   document.body.classList.add("resize-animation-stopper");
